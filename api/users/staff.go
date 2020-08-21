c34d3f5489f064b4590c8b3caf322b732cdcf7ba
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-08-21 18:06:25 +0200

    Retrieve now Name/Avatar/Mail/Description staff

--- api/users/staff.go
+++ api/users/staff.go
@@ -1,8 +1,10 @@
-package users
+package main
 
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"strings"
+	"time"
 )
 
 type Employee struct {
@@ -14,14 +16,31 @@ type Employee struct {
 
 func GetEmployees() []Employee {
 	var employees []Employee
-	c := colly.NewCollector(colly.MaxDepth(1))
+	c := colly.NewCollector(
+		colly.MaxDepth(2))
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		RandomDelay: 3 * time.Second,
+	})
 
 	c.OnRequest(func(request *colly.Request) {
 		fmt.Println("Requesting on", request.URL)
 	})
 
-	c.OnHTML("h2", func(element *colly.HTMLElement) {
-		employees = append(employees, Employee{fullName: element.Text})
+	c.OnHTML(".article_content", func(element *colly.HTMLElement) {
+		var links string
+		if strings.Contains(element.ChildAttr("a", "href"), "mailto") {
+			links := element.ChildAttr("a", "href")
+			links = strings.ReplaceAll(links, "mailto:", "")
+		}
+
+		employees = append(employees, Employee{
+			fullName:    element.ChildText("h2"),
+			mailAdress:  links,
+			profilePic:  "https://camelia55.meuse.fr" + element.ChildAttr("img", "src"),
+			description: element.ChildText(".sitotheque p"),
+		})
 	})
 
 	c.OnError(func(response *colly.Response, err error) {
